e7e1e6605c3f0b503d882cab18d30d4a97b472e0
adamahojahoj <23ib01_cermak@spsjc.cz>
2026-01-28 10:52:03 +0100

    Přidejte soubory projektu.

---
+++ Serialization2/Models/Obed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serialization2.Models
+{
+    public class Obed
+    {
+        public int Id { get; set; }
+        public string Nazev { get; set; }
+    }
+}
