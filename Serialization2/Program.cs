908278da2d9f1bf0ad3dd137fd2048ab6b98b51a
adamahojahoj <23ib01_cermak@spsjc.cz>
2026-01-28 11:46:04 +0100

    dasd

--- Serialization2/Program.cs
+++ Serialization2/Program.cs
@@ -7,13 +7,20 @@ internal class Program
     private static void Main(string[] args)
     {
         Worker.Serializer<Obed> serializer = new();
-        string nazev = InputHelper.InputText("Set name of lunch");
-        int id = InputHelper.InputNumber("Insert ID number");
-        Obed o = new Obed { Id = id, Nazev = nazev };
+        List<Obed> Obedy = new();
+        bool ContinueAdding = true;
+        while (ContinueAdding)
+        {
+            string nazev = InputHelper.InputText("Set name of lunch");
+            int id = InputHelper.InputNumber("Insert ID number");
+            Obed o = new Obed { Id = id, Nazev = nazev };
+            Obedy.Add(o);
+            ContinueAdding = InputHelper.InputText("Would you like to add next one?[y/n]") == "y";
+        }
         string decision1 = InputHelper.InputText("Would you like to serialize?[y/n]");
         if (decision1.Trim().ToLower().Equals("y"))
         {
-            if (serializer.Serialize(o))
+            if (serializer.Serialize(Obedy))
             {
                 Console.WriteLine("Done");
             }
--- Serialization2/Serializer/Serializer.cs
+++ Serialization2/Serializer/Serializer.cs
@@ -3,13 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 
 namespace Serialization2.Serializer
 {
     public class Serializer<T> where T : class
     {
+        private JsonSerializerOptions _options;
+
+        public Serializer()
+        {
+            _options = new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.Create(
+                    UnicodeRanges.BasicLatin,
+                    UnicodeRanges.Latin1Supplement,
+                    UnicodeRanges.LatinExtendedA
+                ),
+                WriteIndented = true
+            };
+        }
 
         public bool Serialize(in T inputData, string path = "Data.json")
         {
@@ -35,7 +51,7 @@ namespace Serialization2.Serializer
         {
             try
             {
-                string json = JsonSerializer.Serialize(collection);
+                string json = JsonSerializer.Serialize(collection,_options);
                 using var stream = new StreamWriter(path);
                 stream.Write(json);
                 stream.Flush();
